0620336e03631f6907c2493baa10c5a969981ae1
Asmita Karmakar <asmitak@cs.utexas.edu>
2022-05-02 21:54:46 -0500

    ethan pls help us

--- kernel/mouse.cc
+++ kernel/mouse.cc
@@ -6,29 +6,31 @@
 
 
 void MOUSE::init(void) {
-    IDT::interrupt(10, (uint32_t)_mouseHandler);
+    IDT::interrupt(12, (uint32_t)_mouseHandler);
+    outb(0x2C,0xfd);
     // Address the 2nd device
-    outb(0x64, 0xd4);
-    outb(0x60, 0xF4);
-    while (inb(0x60) != 0xFA); /* Wait for ACK from mouse... */
-    /* Tell mouse to enable interrupts (IRQ12) */
-    outb(0x64, 0x20);
+    // outb(0x64, 0xD4);
+    // outb(0x60, 0xF4);
+    // while (inb(0x60) != 0xFA); /* Wait for ACK from mouse... */
+    // /* Tell mouse to enable interrupts (IRQ12) */
+    // outb(0x64, 0x20);
 
-    unsigned char res = inb(0x60);
-    res |= 1 << 1;
+    // unsigned char res = inb(0x60);
+    // res |= 1 << 1;
 
-    outb(0x64, 0x60);
-    outb(0x60, res);
+    // outb(0x64, 0x60);
+    // outb(0x60, res);
 }
 
 extern "C" void mouseHandler(uint32_t* things) {
    // Debug::printf("hit mouse handler\n");
     // // uint32_t y = 20;
-    inb(0x64);
+    inb(0x60);
     outb(0x20, 0x20);
-   char *VGA = (char*)0xA0000;
+    VGA_color = 0x01;
+    char *VGA = (char*)0xA0000;
     for (int i = 0; i < 3200; i++) {
-        VGA[i] = VGA_color;
+        VGA[i] = 0x01;
     }
     // char *VGA = (char*)0xA0000;
     // for (int i = 0; i < 3200; i++) {
