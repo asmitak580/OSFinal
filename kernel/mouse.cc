d85409acfcbe8822e1175566d05489c4f3c73198
Alekhya Kuchimanchi <alekhyak@cs.utexas.edu>
2022-05-02 19:44:05 -0500

    mouse?

--- kernel/mouse.cc
+++ kernel/mouse.cc
@@ -6,9 +6,19 @@
 
 
 void MOUSE::init(void) {
-   IDT::interrupt(10, (uint32_t)_mouseHandler);
-   outb(0x21,0xFD);
-   
+    IDT::interrupt(10, (uint32_t)_mouseHandler);
+    // Address the 2nd device
+    outb(0x64, 0xd4);
+    outb(0x60, 0xF4);
+    while (inb(0x60) != 0xFA); /* Wait for ACK from mouse... */
+    /* Tell mouse to enable interrupts (IRQ12) */
+    outb(0x64, 0x20);
+
+    unsigned char res = inb(0x60);
+    res |= 1 << 1;
+
+    outb(0x64, 0x60);
+    outb(0x60, res);
 }
 
 extern "C" void mouseHandler(uint32_t* things) {
